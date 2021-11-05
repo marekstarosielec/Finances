fcc220c30bb4723613458cf50274078675244ff8
marekstarosielec <git@starosielec.net>
2021-11-05 22:51:39 +0100

    Simplified transaction details

--- AngularClient/src/app/pages/transactions/transaction.component.ts
+++ AngularClient/src/app/pages/transactions/transaction.component.ts
@@ -8,82 +8,82 @@ import { take } from 'rxjs/operators';
 import {v4 as uuidv4} from 'uuid';
 import { Location } from '@angular/common';
 
-interface TransactionHeader {
-    column: string;
-    showInBankInfo: boolean;
-    title: string;
-    isError?: boolean;
-    value?: string;
-}
+// interface TransactionHeader {
+//     column: string;
+//     showInBankInfo: boolean;
+//     title: string;
+//     isError?: boolean;
+//     value?: string;
+// }
 
-const TransactionHeaders: TransactionHeader[] = [
-    { column: 'scrappingDate', showInBankInfo: false, title: 'Data scrapowania' },
-    { column: 'status', showInBankInfo: true, title: 'Status' },
-    { column: 'tags', showInBankInfo: true, title: 'Tagi' },
-    { column: 'to', showInBankInfo: true, title: 'Do' },
-    { column: 'sellerCity', showInBankInfo: true, title: 'Miasto sprzedawcy' },
-    { column: 'sellerCountry', showInBankInfo: true, title: 'Data operacji' },
-    { column: 'operationDate', showInBankInfo: true, title: 'Z rachunku powiązanego' },
-    { column: 'fromConnectedAccount', showInBankInfo: true, title: 'Z rachunku powiązanego' },
-    { column: 'debitCardNo', showInBankInfo: true, title: 'Numer karty debetowej' },
-    { column: 'type', showInBankInfo: true, title: 'Rodzaj operacji' },
-    { column: 'operationDescription', showInBankInfo: true, title: 'Opis operacji' },
-    { column: 'name', showInBankInfo: true, title: 'Nazwa odbiorcy' },
-    { column: 'originalCurrencyAmount', showInBankInfo: true, title: 'Kwota w walucie rozliczeniowej' },
-    { column: 'settlementCurrencyAmount', showInBankInfo: true, title: 'Kwota w walucie rachunku' },
-    { column: 'accountCurrencyAmount', showInBankInfo: true, title: '' },
-    { column: 'afterOperationBalance', showInBankInfo: true, title: 'Saldo po operacji' },
-    { column: 'settlementDate', showInBankInfo: true, title: 'Data rozliczenia' },
-    { column: 'operationNumber', showInBankInfo: true, title: 'Numer operacji' },
-    { column: 'puchaseByCardFrom', showInBankInfo: true, title: 'Zakup przy użyciu karty od' },
-    { column: 'transactionPlace', showInBankInfo: true, title: 'Miejsce transakcji' },
-    { column: 'transactionTitle', showInBankInfo: true, title: 'Tytuł przelewu' },
-    { column: 'fromSenderAccount', showInBankInfo: true, title: 'Z rachunku nadawcy' },
-    { column: 'sender', showInBankInfo: true, title: 'Nadawca' },
-    { column: 'senderAddress', showInBankInfo: true, title: 'Adres nadawcy' },
-    { column: 'senderCity', showInBankInfo: true, title: 'Miejscowość nadawcy' },
-    { column: 'toAccount', showInBankInfo: true, title: 'Na rachunek' },
-    { column: 'receiverBank', showInBankInfo: true, title: 'Bank odbiorcy' },
-    { column: 'receiverAddress', showInBankInfo: true, title: 'Adres odbiorcy' },
-    { column: 'receiverCity', showInBankInfo: true, title: 'Miejscowość odbiorcy' },
-    { column: 'operationAmount', showInBankInfo: true, title: 'Kwota operacji' },
-    { column: 'bookingDate', showInBankInfo: true, title: 'Data księgowania' },
-    { column: 'operationTitle', showInBankInfo: true, title: 'Tytuł operacji' },
-    { column: 'ownAccount', showInBankInfo: true, title: 'Konto własne' },
-    { column: 'scrapID', showInBankInfo: false, title: 'ScrapID' },
-    { column: 'newScrap', showInBankInfo: false, title: 'NewScrap' },
-    { column: 'senderName', showInBankInfo: true, title: 'Nazwa nadawcy' },
-    { column: 'senderBank', showInBankInfo: true, title: 'Bank nadawcy' },
-    { column: 'innerTransferGoingFrom', showInBankInfo: true, title: 'Przelew wewnętrzny wychodzący od' },
-    { column: 'outerTransferGoingFrom', showInBankInfo: true, title: 'Przelew zewnętrzny wychodzący od' },
-    { column: 'transactionDate', showInBankInfo: true, title: 'Data transakcji' },
-    { column: 'ownTransferFrom', showInBankInfo: true, title: 'Przelew własny od' },
-    { column: 'mTransferGoingFrom', showInBankInfo: true, title: 'Przelew mTransfer wychodzący od' },
-    { column: 'innerTransferComingFrom', showInBankInfo: true, title: 'Przelew wewnętrzny przychodzący od' },
-    { column: 'savingTransfer', showInBankInfo: true, title: 'Przelew na twoje cele od' },
-    { column: 'cashWithdrawal', showInBankInfo: true, title: 'Wypłata gotówki kartą' },
-    { column: 'nationalATMCommision', showInBankInfo: true, title: 'Prowizja-wypłata bankomat krajowy od' },
-    { column: 'outerTransferComingFrom', showInBankInfo: true, title: 'Przelew zewnętrzny przychodzący od' },
-    { column: 'original', showInBankInfo: false, title: 'Oryginał' },
-    { column: 'transactionText', showInBankInfo: true, title: 'Tekst tranzakcji' },
-    { column: 'accountName', showInBankInfo: false, title: 'Nazwa konta' },
-    { column: 'referenceNumber', showInBankInfo: true, title: 'Numer referencyjny' },
-    { column: 'transactionAmount', showInBankInfo: true, title: 'Kwota przelewu' },
-    { column: 'senderAccount', showInBankInfo: true, title: 'Rachunek nadawcy' },
-    { column: 'commisionsPaidBy', showInBankInfo: true, title: 'Prowizje i opłaty pokrywa' },
-    { column: 'receiver', showInBankInfo: true, title: 'Odbiorca' },
-    { column: 'dateAndTimeOfReceiving', showInBankInfo: true, title: 'Data i godzina otrzymania' },
-    { column: 'purchaseByCardPhone', showInBankInfo: true, title: 'Zakup przy użyciu karty - transakcja telefonem od' },
-    { column: 'date', showInBankInfo: false, title: 'Data' },
-    { column: 'account', showInBankInfo: false, title: 'Konto' },
-    { column: 'category', showInBankInfo: false, title: 'Kategoria' },
-    { column: 'amount', showInBankInfo: false, title: 'Kwota' },
-    { column: 'description', showInBankInfo: false, title: 'Opis' },
-    { column: 'comment', showInBankInfo: false, title: 'Komentarz' },
-    { column: 'invoice', showInBankInfo: false, title: 'Faktura' },
-    { column: 'details', showInBankInfo: false, title: 'Szczegóły' },
-    { column: 'person', showInBankInfo: false, title: 'Osoba' }
-]
+// const TransactionHeaders: TransactionHeader[] = [
+//     { column: 'scrappingDate', showInBankInfo: false, title: 'Data scrapowania' },
+//     { column: 'status', showInBankInfo: true, title: 'Status' },
+//     { column: 'tags', showInBankInfo: true, title: 'Tagi' },
+//     { column: 'to', showInBankInfo: true, title: 'Do' },
+//     { column: 'sellerCity', showInBankInfo: true, title: 'Miasto sprzedawcy' },
+//     { column: 'sellerCountry', showInBankInfo: true, title: 'Data operacji' },
+//     { column: 'operationDate', showInBankInfo: true, title: 'Z rachunku powiązanego' },
+//     { column: 'fromConnectedAccount', showInBankInfo: true, title: 'Z rachunku powiązanego' },
+//     { column: 'debitCardNo', showInBankInfo: true, title: 'Numer karty debetowej' },
+//     { column: 'type', showInBankInfo: true, title: 'Rodzaj operacji' },
+//     { column: 'operationDescription', showInBankInfo: true, title: 'Opis operacji' },
+//     { column: 'name', showInBankInfo: true, title: 'Nazwa odbiorcy' },
+//     { column: 'originalCurrencyAmount', showInBankInfo: true, title: 'Kwota w walucie rozliczeniowej' },
+//     { column: 'settlementCurrencyAmount', showInBankInfo: true, title: 'Kwota w walucie rachunku' },
+//     { column: 'accountCurrencyAmount', showInBankInfo: true, title: '' },
+//     { column: 'afterOperationBalance', showInBankInfo: true, title: 'Saldo po operacji' },
+//     { column: 'settlementDate', showInBankInfo: true, title: 'Data rozliczenia' },
+//     { column: 'operationNumber', showInBankInfo: true, title: 'Numer operacji' },
+//     { column: 'puchaseByCardFrom', showInBankInfo: true, title: 'Zakup przy użyciu karty od' },
+//     { column: 'transactionPlace', showInBankInfo: true, title: 'Miejsce transakcji' },
+//     { column: 'transactionTitle', showInBankInfo: true, title: 'Tytuł przelewu' },
+//     { column: 'fromSenderAccount', showInBankInfo: true, title: 'Z rachunku nadawcy' },
+//     { column: 'sender', showInBankInfo: true, title: 'Nadawca' },
+//     { column: 'senderAddress', showInBankInfo: true, title: 'Adres nadawcy' },
+//     { column: 'senderCity', showInBankInfo: true, title: 'Miejscowość nadawcy' },
+//     { column: 'toAccount', showInBankInfo: true, title: 'Na rachunek' },
+//     { column: 'receiverBank', showInBankInfo: true, title: 'Bank odbiorcy' },
+//     { column: 'receiverAddress', showInBankInfo: true, title: 'Adres odbiorcy' },
+//     { column: 'receiverCity', showInBankInfo: true, title: 'Miejscowość odbiorcy' },
+//     { column: 'operationAmount', showInBankInfo: true, title: 'Kwota operacji' },
+//     { column: 'bookingDate', showInBankInfo: true, title: 'Data księgowania' },
+//     { column: 'operationTitle', showInBankInfo: true, title: 'Tytuł operacji' },
+//     { column: 'ownAccount', showInBankInfo: true, title: 'Konto własne' },
+//     { column: 'scrapID', showInBankInfo: false, title: 'ScrapID' },
+//     { column: 'newScrap', showInBankInfo: false, title: 'NewScrap' },
+//     { column: 'senderName', showInBankInfo: true, title: 'Nazwa nadawcy' },
+//     { column: 'senderBank', showInBankInfo: true, title: 'Bank nadawcy' },
+//     { column: 'innerTransferGoingFrom', showInBankInfo: true, title: 'Przelew wewnętrzny wychodzący od' },
+//     { column: 'outerTransferGoingFrom', showInBankInfo: true, title: 'Przelew zewnętrzny wychodzący od' },
+//     { column: 'transactionDate', showInBankInfo: true, title: 'Data transakcji' },
+//     { column: 'ownTransferFrom', showInBankInfo: true, title: 'Przelew własny od' },
+//     { column: 'mTransferGoingFrom', showInBankInfo: true, title: 'Przelew mTransfer wychodzący od' },
+//     { column: 'innerTransferComingFrom', showInBankInfo: true, title: 'Przelew wewnętrzny przychodzący od' },
+//     { column: 'savingTransfer', showInBankInfo: true, title: 'Przelew na twoje cele od' },
+//     { column: 'cashWithdrawal', showInBankInfo: true, title: 'Wypłata gotówki kartą' },
+//     { column: 'nationalATMCommision', showInBankInfo: true, title: 'Prowizja-wypłata bankomat krajowy od' },
+//     { column: 'outerTransferComingFrom', showInBankInfo: true, title: 'Przelew zewnętrzny przychodzący od' },
+//     { column: 'original', showInBankInfo: false, title: 'Oryginał' },
+//     { column: 'transactionText', showInBankInfo: true, title: 'Tekst tranzakcji' },
+//     { column: 'accountName', showInBankInfo: false, title: 'Nazwa konta' },
+//     { column: 'referenceNumber', showInBankInfo: true, title: 'Numer referencyjny' },
+//     { column: 'transactionAmount', showInBankInfo: true, title: 'Kwota przelewu' },
+//     { column: 'senderAccount', showInBankInfo: true, title: 'Rachunek nadawcy' },
+//     { column: 'commisionsPaidBy', showInBankInfo: true, title: 'Prowizje i opłaty pokrywa' },
+//     { column: 'receiver', showInBankInfo: true, title: 'Odbiorca' },
+//     { column: 'dateAndTimeOfReceiving', showInBankInfo: true, title: 'Data i godzina otrzymania' },
+//     { column: 'purchaseByCardPhone', showInBankInfo: true, title: 'Zakup przy użyciu karty - transakcja telefonem od' },
+//     { column: 'date', showInBankInfo: false, title: 'Data' },
+//     { column: 'account', showInBankInfo: false, title: 'Konto' },
+//     { column: 'category', showInBankInfo: false, title: 'Kategoria' },
+//     { column: 'amount', showInBankInfo: false, title: 'Kwota' },
+//     { column: 'description', showInBankInfo: false, title: 'Opis' },
+//     { column: 'comment', showInBankInfo: false, title: 'Komentarz' },
+//     { column: 'invoice', showInBankInfo: false, title: 'Faktura' },
+//     { column: 'details', showInBankInfo: false, title: 'Szczegóły' },
+//     { column: 'person', showInBankInfo: false, title: 'Osoba' }
+// ]
 
 @Component({
     selector: 'transaction',
@@ -130,22 +130,22 @@ export class TransactionComponent implements OnInit, OnDestroy{
         this.routeSubscription.unsubscribe();
    }
 
-   getBankInfo(transaction:Transaction): TransactionHeader[] {
-        if(!transaction)
-            return;
-        let result: TransactionHeader[] = [];
-        Object.keys(transaction).forEach(key => {
-            let header = TransactionHeaders.find(th => th.column === key);
-            if (!header){
-                header = { column: key, title: key, isError: true, showInBankInfo: true };
-            }
-            if (transaction[key] && transaction[key] !== '' && header.showInBankInfo) {
-                header.value = transaction[key];
-                result.push(header);
-            }
-        });
-        return result;
-    }
+//    getBankInfo(transaction:Transaction): TransactionHeader[] {
+//         if(!transaction)
+//             return;
+//         let result: TransactionHeader[] = [];
+//         Object.keys(transaction).forEach(key => {
+//             let header = TransactionHeaders.find(th => th.column === key);
+//             if (!header){
+//                 header = { column: key, title: key, isError: true, showInBankInfo: true };
+//             }
+//             if (transaction[key] && transaction[key] !== '' && header.showInBankInfo) {
+//                 header.value = transaction[key];
+//                 result.push(header);
+//             }
+//         });
+//         return result;
+//     }
 
 
     isSavable(): boolean {
--- FinancesApi/Models/Transaction.cs
+++ FinancesApi/Models/Transaction.cs
@@ -8,173 +8,167 @@
         //Status	
         public string Status { get; set; }
 
-        //Tagi
-        public string Tags { get; set; }
+        ////Do
+        //public string To { get; set; }
 
-        //Do
-        public string To { get; set; }
+        ////Miasto sprzedawcy
+        //public string SellerCity { get; set; }
 
-        //Miasto sprzedawcy
-        public string SellerCity { get; set; }
+        ////Kraj sprzedawcy
+        //public string SellerCountry { get; set; }
 
-        //Kraj sprzedawcy
-        public string SellerCountry { get; set; }
+        ////Data operacji
+        //public string OperationDate { get; set; }
 
-        //Data operacji
-        public string OperationDate { get; set; }
+        ////Z rachunku powiązanego
+        //public string FromConnectedAccount { get; set; }
 
-        //Z rachunku powiązanego
-        public string FromConnectedAccount { get; set; }
+        ////Rodzaj operacji
+        //public string Type { get; set; }
 
-        //Numer karty debetowej
-        public string DebitCardNo { get; set; }
+        ////Opis operacji
+        //public string OperationDescription { get; set; }
 
-        //Rodzaj operacji
-        public string Type { get; set; }
+        ////Nazwa odbiorcy
+        //public string Name { get; set; }
 
-        //Opis operacji
-        public string OperationDescription { get; set; }
+        ////Kwota w walucie oryginalnej
+        //public string OriginalCurrencyAmount { get; set; }
 
-        //Nazwa odbiorcy
-        public string Name { get; set; }
+        ////Kwota w walucie rozliczeniowej
+        //public string SettlementCurrencyAmount { get; set; }
 
-        //Kwota w walucie oryginalnej
-        public string OriginalCurrencyAmount { get; set; }
+        ////Kwota w walucie rachunku
+        //public string AccountCurrencyAmount { get; set; }
 
-        //Kwota w walucie rozliczeniowej
-        public string SettlementCurrencyAmount { get; set; }
+        ////Saldo po operacji
+        //public string AfterOperationBalance { get; set; }
 
-        //Kwota w walucie rachunku
-        public string AccountCurrencyAmount { get; set; }
+        ////Data rozliczenia
+        //public string SettlementDate { get; set; }
 
-        //Saldo po operacji
-        public string AfterOperationBalance { get; set; }
+        ////Numer operacji
+        //public string OperationNumber { get; set; }
 
-        //Data rozliczenia
-        public string SettlementDate { get; set; }
+        ////Zakup przy użyciu karty od
+        //public string PuchaseByCardFrom { get; set; }
 
-        //Numer operacji
-        public string OperationNumber { get; set; }
+        ////Miejsce transakcji
+        //public string TransactionPlace { get; set; }
 
-        //Zakup przy użyciu karty od
-        public string PuchaseByCardFrom { get; set; }
+        ////Tytuł przelewu
+        //public string TransactionTitle { get; set; }
 
-        //Miejsce transakcji
-        public string TransactionPlace { get; set; }
+        ////Z rachunku nadawcy
+        //public string FromSenderAccount { get; set; }
 
-        //Tytuł przelewu
-        public string TransactionTitle { get; set; }
+        ////Nadawca
+        //public string Sender { get; set; }
 
-        //Z rachunku nadawcy
-        public string FromSenderAccount { get; set; }
+        ////Adres nadawcy
+        //public string SenderAddress { get; set; }
 
-        //Nadawca
-        public string Sender { get; set; }
+        ////Miejscowość nadawcy
+        //public string SenderCity { get; set; }
 
-        //Adres nadawcy
-        public string SenderAddress { get; set; }
+        ////Na rachunek
+        //public string ToAccount { get; set; }
 
-        //Miejscowość nadawcy
-        public string SenderCity { get; set; }
+        ////Bank odbiorcy
+        //public string ReceiverBank { get; set; }
 
-        //Na rachunek
-        public string ToAccount { get; set; }
+        ////Adres odbiorcy
+        //public string ReceiverAddress { get; set; }
 
-        //Bank odbiorcy
-        public string ReceiverBank { get; set; }
+        ////Miejscowość odbiorcy
+        //public string ReceiverCity { get; set; }
 
-        //Adres odbiorcy
-        public string ReceiverAddress { get; set; }
+        ////Kwota operacji
+        //public string OperationAmount { get; set; }
 
-        //Miejscowość odbiorcy
-        public string ReceiverCity { get; set; }
+        ////Data księgowania
+        //public string BookingDate { get; set; }
 
-        //Kwota operacji
-        public string OperationAmount { get; set; }
+        ////Tytuł operacji
+        //public string OperationTitle { get; set; }
 
-        //Data księgowania
-        public string BookingDate { get; set; }
-
-        //Tytuł operacji
-        public string OperationTitle { get; set; }
-
-        //Konto własne
-        public string OwnAccount { get; set; }
+        ////Konto własne
+        //public string OwnAccount { get; set; }
 
         //ScrapID
         public string ScrapID { get; set; }
 
-        //NewScrap
-        public int? NewScrap { get; set; }
+        ////NewScrap
+        //public int? NewScrap { get; set; }
     
-        //Nazwa nadawcy
-        public string SenderName { get; set; }
+        ////Nazwa nadawcy
+        //public string SenderName { get; set; }
 
-        //Bank nadawcy
-        public string SenderBankName { get; set; }
+        ////Bank nadawcy
+        //public string SenderBankName { get; set; }
 
-        //Przelew wewnętrzny wychodzący od
-        public string InnerTransferGoingFrom { get; set; }
+        ////Przelew wewnętrzny wychodzący od
+        //public string InnerTransferGoingFrom { get; set; }
 
-        //Przelew zewnętrzny wychodzący od
-        public string OuterTransferGoingFrom { get; set; }
+        ////Przelew zewnętrzny wychodzący od
+        //public string OuterTransferGoingFrom { get; set; }
 
-        //Data transakcji
-        public string TransactionDate { get; set; }
+        ////Data transakcji
+        //public string TransactionDate { get; set; }
 
-        //Przelew własny od
-        public string OwnTransferFrom { get; set; }
+        ////Przelew własny od
+        //public string OwnTransferFrom { get; set; }
 
-        //Przelew mTransfer wychodzący od
-        public string mTransferGoingFrom { get; set; }
+        ////Przelew mTransfer wychodzący od
+        //public string mTransferGoingFrom { get; set; }
 
-        //Przelew wewnętrzny przychodzący od
-        public string InnerTransferComingFrom { get; set; }
+        ////Przelew wewnętrzny przychodzący od
+        //public string InnerTransferComingFrom { get; set; }
 
-        //Przelew na twoje cele od
-        public string SavingTransfer { get; set; }
+        ////Przelew na twoje cele od
+        //public string SavingTransfer { get; set; }
 
-        //Wypłata gotówki kartą
-        public string CashWithdrawal { get; set; }
+        ////Wypłata gotówki kartą
+        //public string CashWithdrawal { get; set; }
 
-        //Prowizja-wypłata bankomat krajowy od
-        public string NationalATMCommision { get; set; }
+        ////Prowizja-wypłata bankomat krajowy od
+        //public string NationalATMCommision { get; set; }
 
-        //Przelew zewnętrzny przychodzący od
-        public string OuterTransferComingFrom { get; set; }
+        ////Przelew zewnętrzny przychodzący od
+        //public string OuterTransferComingFrom { get; set; }
 
-        //Oryginał
-        public string Original { get; set; }
+        ////Oryginał
+        //public string Original { get; set; }
 
-        //Tekst tranzakcji
-        public string TransactionText { get; set; }
+        ////Tekst tranzakcji
+        //public string TransactionText { get; set; }
 
-        //Nazwa konta
-        public string AccountName { get; set; }
+        ////Nazwa konta
+        //public string AccountName { get; set; }
 
-        //Numer referencyjny
-        public int? ReferenceNumber { get; set; }
+        ////Numer referencyjny
+        //public int? ReferenceNumber { get; set; }
 
-        //Kwota przelewu
-        public string TransactionAmount { get; set; }
+        ////Kwota przelewu
+        //public string TransactionAmount { get; set; }
 
-        //Rachunek nadawcy
-        public string SenderAccount { get; set; }
+        ////Rachunek nadawcy
+        //public string SenderAccount { get; set; }
 
-        //Prowizje i opłaty pokrywa
-        public string CommisionsPaidBy { get; set; }
+        ////Prowizje i opłaty pokrywa
+        //public string CommisionsPaidBy { get; set; }
 
-        //Odbiorca
-        public string Receiver { get; set; }
+        ////Odbiorca
+        //public string Receiver { get; set; }
 
-        //Data i godzina otrzymania
-        public string DateAndTimeOfReceiving { get; set; }
+        ////Data i godzina otrzymania
+        //public string DateAndTimeOfReceiving { get; set; }
 
-        //Zakup przy użyciu karty - transakcja telefonem od
-        public string PurchaseByCardPhone { get; set; }
+        ////Zakup przy użyciu karty - transakcja telefonem od
+        //public string PurchaseByCardPhone { get; set; }
 
-        //Reserved18
-        public string Reserved18 { get; set; }
+        ////Reserved18
+        //public string Reserved18 { get; set; }
 
         //Data
         public string Date { get; set; }
@@ -194,8 +188,8 @@
         //Komentarz
         public string Comment { get; set; }
 
-        //Faktura
-        public string Invoice { get; set; }
+        ////Faktura
+        //public string Invoice { get; set; }
 
         //Szczegóły
         public string Details { get; set; }
@@ -203,6 +197,5 @@
         //Osoba
         public string Person { get; set; }
 
-        //public decimal Amount2 { get; set; }
     }
 }
