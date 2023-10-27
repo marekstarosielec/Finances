3b884d7e47446a83e52352b765077295713834ce
Domowy <Domowy@DOMOWY>
2023-10-27 21:16:17 +0200

    Working on dynami component for filters.

---
+++ FinancesBlazor/DataTypes/ComponentMetadata.cs
@@ -0,0 +1,14 @@
+﻿namespace FinancesBlazor.DataTypes;
+
+public class ComponentMetadata
+{
+    public Type Type { get; set; }
+    public string? Name { get; set; }
+    public Dictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>();
+
+    public ComponentMetadata(Type type)
+    {
+        Type = type;
+    }
+
+}
