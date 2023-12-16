3e2276c2e630914fb4dc99f29d0251924f9b9e4c
Domowy <Domowy@DOMOWY>
2023-12-16 22:33:29 +0100

    Reading data from details.

--- DataSource/DataQueryResult.cs
+++ DataSource/DataQueryResult.cs
@@ -6,10 +6,21 @@ public class DataQueryResult
     public IEnumerable<Dictionary<DataColumn, object?>> Rows { get; }
     public int TotalRowCount { get; }
 
+    private DataColumn? IdColumn;
+
     public DataQueryResult(IEnumerable<DataColumn> columns, IEnumerable<Dictionary<DataColumn, object?>> rows, int totalRowCount)
     {
         Columns = columns;
         Rows = rows;
         TotalRowCount = totalRowCount;
+        IdColumn = Columns.FirstOrDefault(c => c.ColumnName == "Id");
+    }
+
+    public Dictionary<DataColumn, object?>? GetById(string id)
+    {
+        if (IdColumn == null)
+            throw new InvalidOperationException("Column Id not present in data source");
+
+        return Rows.FirstOrDefault(r => r[IdColumn]?.ToString() == id);
     }
 }
